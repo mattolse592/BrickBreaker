4c475fdef98d30290950fcf7a3578ccc1907248c
valemont800 <valmon13@hotmail.com>
2024-04-25 09:36:50 -0400

    aa

--- BrickBreaker/Screens/GameScreen.Designer.cs
+++ BrickBreaker/Screens/GameScreen.Designer.cs
@@ -42,9 +42,10 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Britannic Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.SaddleBrown;
             this.button1.Location = new System.Drawing.Point(1156, 20);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.Size = new System.Drawing.Size(75, 33);
             this.button1.TabIndex = 0;
             this.button1.Text = "Exit";
             this.button1.UseVisualStyleBackColor = true;
