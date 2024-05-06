8951eb2a5a3041e859b716ce01407e94a1b14ce4
Derick James <Derick James@THE-PICKLE-PAD>
2024-05-06 09:29:54 -0400

    ball do funky shit bruv

--- BrickBreaker/Screens/GameScreen.cs
+++ BrickBreaker/Screens/GameScreen.cs
@@ -139,12 +139,23 @@ namespace BrickBreaker
                         }
 
                         int mag = (int)Math.Sqrt(Math.Pow(ball.y + 2, 2) + Math.Pow(ball.x - ball.throwX, 2));
-                        float yScale = ((ball.y + 2) / mag);
-                        float xScale = ((ball.x - ball.throwX) / mag);
+                        float yScale = (((float)ball.y + 2) / mag);
+                        float xScale = (((float)ball.x - ball.throwX) / mag);
                         
+                        if (xScale < 0)
+                        {
+                            xScale *= -1;
+                        }
 
-                        ball.defaultSpeedX = 0;
-                        ball.defaultSpeedY = 6;
+                        ball.xSpeed = Math.Abs(ball.xSpeed); ;
+
+                        ball.defaultSpeedX = (int)(6 * xScale);
+                        ball.defaultSpeedY = (int)(6 * yScale);
+
+                        if (ball.x + (ball.size /2) > ball.throwX)
+                        {
+                            ball.xSpeed *= -1;
+                        }
                     }
                     break;
                 case Keys.F:
@@ -202,14 +213,11 @@ namespace BrickBreaker
             if (ball.BottomCollision(this))
             {
 
-
-
                 stick = true;
 
 
                 // Moves the ball back to origin
-                ball.x = ((paddle.x - (ball.size / 2)) + (paddle.width / 2));
-                ball.y = (this.Height - paddle.height) - 85;
+              
             }
 
             // Check for collision of ball with paddle, (incl. paddle movement)
