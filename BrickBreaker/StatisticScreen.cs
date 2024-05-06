cdbb4d39e1e3ffa5e0edca57c5c6ab5787455c68
Matthew Olsen <mattolse592@ed.amdsb.ca>
2024-05-06 09:30:51 -0400

    yay

--- BrickBreaker/StatisticScreen.cs
+++ BrickBreaker/StatisticScreen.cs
@@ -12,9 +12,20 @@ namespace BrickBreaker
 {
     public partial class StatisticScreen : UserControl
     {
+        
         public StatisticScreen()
         {
             InitializeComponent();
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            Form1.ChangeScreen(this, new GameScreen());
+        }
+
+        private void backButton_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawLine()
         }
     }
 }
